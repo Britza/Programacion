04e0a07ff43b7d9503be3668bebcd803b857105b
britza <bzegarraflores@danielcastelao.org>
2020-11-19 13:38:23 +0100

    Primera prueba

--- src/com/programacion/boletin1/Ejercicio1.java
+++ src/com/programacion/boletin1/Ejercicio1.java
@@ -2,6 +2,6 @@ package com.programacion.boletin1;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Ejercicio 1 Boletrin 1");
+        System.out.println("Ejercicio 1  1");
     }
 }
