36ec3960d7b8836ce63ac91e5334fb0e495815a4
britza <bzegarraflores@danielcastelao.org>
2020-12-13 01:56:34 +0100

    boletines

---
+++ src/com/programacion/Boletin3/Ejercicio2.java
@@ -0,0 +1,18 @@
+package com.programacion.Boletin3;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float temperatura, kelvin, F;
+        System.out.println("Introduce o valor da temperatura en grados centigrados :");
+        temperatura=sc.nextFloat();
+        kelvin=temperatura+273;
+        F=(float) 9/5*temperatura+32;
+        System.out.println("A temperatura en grados kelvin é " + kelvin +" k"+"\nA temperatura en grados fahrenheit é "+F+" f");
+
+
+    }
+}
