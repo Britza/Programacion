98020628dde82db0f763c87439a2ade9cdb722c5
britza <bzegarraflores@danielcastelao.org>
2020-12-13 02:29:30 +0100

    boletines

---
+++ src/com/programacion/Boletin2/Boletin2.java
@@ -0,0 +1,11 @@
+package com.programacion.Boletin2;
+
+public class Boletin2 {
+
+    public static void main(String[] args) {
+        double lado = 3;
+        double area = lado * lado;
+        System.out.println(area);
+
+    }
+}
