71105fc52ed434973599275105d3600e642c2480
britza <bzegarraflores@danielcastelao.org>
2021-02-03 13:48:55 +0100

    boletines

--- src/com/programacion/Boletin14/Boletin14.java
+++ src/com/programacion/Boletin14/Boletin14.java
@@ -1,10 +1,21 @@
 package com.programacion.Boletin14;
 
+import javax.swing.*;
+
 public class Boletin14 {
 
     public static void main(String[] args) {
 
-    }
+        ConversorTemperaturas obxCT = new ConversorTemperaturas();
+        float centrigrados = Float.parseFloat(JOptionPane.showInputDialog("introduce o valor de la tempertatura"));
+
+        try {
+
+            System.out.println("A temperatura en grados fharenheit: " + obxCT.fharenheit(centrigrados) + "A temperatura en grados reamur: " + obxCT.reamur(centrigrados));
+
+    }catch (TemperaturaErradaExcepcion e){
+            System.out.println(e.toString());
+        }
 
 }
 
--- src/com/programacion/Boletin14/ConversorTemperaturas.java
+++ src/com/programacion/Boletin14/ConversorTemperaturas.java
@@ -2,15 +2,25 @@ package com.programacion.Boletin14;
 
 public class ConversorTemperaturas {
 
-    private int centigrados;
-
     private static final int grados = 80;
 
-    public ConversorTemperaturas(int temperatura) {
-        this.temperatura = temperatura;
+    public float fharenheit(float centigrados) throws TemperaturaErradaExcepcion {
+        float fharenheit;
+        if(centigrados<grados){
+            throw new TemperaturaErradaExcepcion();
+        }
+        else
+            fharenheit = (float) (9.0/5.0 * centigrados + 32.4);
+        return fharenheit;
     }
 
-    public float calcularFharenheir(){
-        return fharenheir= 9.0/5.0 * centigrados;
+    public float reamur(float centigrados)throws TemperaturaErradaExcepcion{
+        float reamur;
+        if(centigrados<grados){
+            throw new TemperaturaErradaExcepcion();
+        }
+        else
+            reamur=(float)(4.0/5.0 * centigrados);
+        return reamur;
     }
 }
