03701a8068ae8968ab5767fae78cd32d1ff48207
britza <bzegarraflores@danielcastelao.org>
2021-03-11 12:10:50 +0100

    boletines

---
+++ src/com/programacion/Boletin16/Boletin16.java
@@ -0,0 +1,4 @@
+package com.programacion.Boletin16;
+
+public class Boletin16 {
+}
---
+++ src/com/programacion/Boletin16/Numeros.java
@@ -0,0 +1,27 @@
+package com.programacion.Boletin16;
+
+public class Numeros {
+
+    private int numero;
+
+    public Numeros() {
+    }
+
+    public Numeros(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Numero=" + numero;
+    }
+
+}
