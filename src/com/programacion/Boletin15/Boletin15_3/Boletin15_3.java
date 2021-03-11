620fab32496a63e85fc25030adc92a17fd6fa62d
britza <bzegarraflores@danielcastelao.org>
2021-03-11 11:50:04 +0100

    boletines

---
+++ src/com/programacion/Boletin15/Boletin15_3/Boletin15_3.java
@@ -0,0 +1,11 @@
+package com.programacion.Boletin15.Boletin15_3;
+
+public class Boletin15_3 {
+
+    public static void main(String[] args) {
+        Metodos15_3 obx = new Metodos15_3();
+        obx.menu();
+
+
+    }
+}
---
+++ src/com/programacion/Boletin15/Boletin15_4/Boletin15_4.java
@@ -0,0 +1,10 @@
+package com.programacion.Boletin15.Boletin15_4;
+
+public class Boletin15_4 {
+    public static void main(String[] args) {
+        Dni dni = new Dni();
+        dni.Letra();
+
+
+    }
+}
