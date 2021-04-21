4503c6b5759e726c73c1a02771b80d5f5b0da1c2
Britza <72441790+Britza@users.noreply.github.com>
2021-04-21 17:28:49 +0200

    Boletin 21

---
+++ boletin21/src/com/bri/boletin21/Boletin21.java
@@ -0,0 +1,15 @@
+
+package com.bri.boletin21;
+
+
+public class Boletin21 {
+
+    
+    public static void main(String[] args) {
+      /* Ventana1 obx = new Ventana1();
+       obx.setVisible(true);*/
+      Ventana2 obx = new Ventana2();
+      obx.setVisible(true);
+    }
+    
+}
